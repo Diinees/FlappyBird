5d2dc9b97dd2fc8e089b10906280bc725dc6fa06
L.R.Carvalho <54295653+Diinees@users.noreply.github.com>
2021-05-10 21:50:26 -0300

    10/05/2021

--- core/src/com/mygdx/game/flappybird/game.java
+++ core/src/com/mygdx/game/flappybird/game.java
@@ -7,29 +7,29 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class game extends ApplicationAdapter {
-
+    //Definindo variavel
     private SpriteBatch batch;
     private Texture passaro;
     private Texture fundo;
-    //definindo variavel para movimentacao//
-
     private int moveY = 0;
     private int moveX = 0;
 
 
-    //variavel para cada dispositivo//
+    //Variavel de dispositivos
     private float larguraDispositivo;
     private float alturaDispositivo;
 
     @Override
     public void create() {
 
-        //definindo as imagens com os assets definidos
+
+        //Definindo imagens
         batch = new SpriteBatch();
         passaro = new Texture("passaro1.png");
         fundo = new Texture("fundo.png");
 
-        //buscando o tamnho do dispositivo e inserindo nas variaveis//
+
+        //Buscando tamnho do dispositivo e inserindo nas variaveis
         larguraDispositivo = Gdx.graphics.getWidth();
         alturaDispositivo = Gdx.graphics.getHeight();
 
@@ -39,7 +39,9 @@ public class game extends ApplicationAdapter {
     public void render() {
 
         batch.begin();
-        //criando fundo. passando altura, largura e diametro para a imagem, incrmentando para adaptacao//
+        //Croacao de fundo
+        // Passando especificacoes de tamanho para a imagem
+        //Incrmentando para adaptacao
         batch.draw(fundo, 0, 0, larguraDispositivo, alturaDispositivo);
         batch.draw(passaro, 50, 50, moveX, moveY);
         moveX++;
